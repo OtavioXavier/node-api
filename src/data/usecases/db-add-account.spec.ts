e202802feb50346ee5b39a488e8d8a2d2808d80c
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-04-21 11:04:12 -0300

    test: garante que AddAccountRepository é chamado com as credenciais corretas

--- src/data/usecases/db-add-account.spec.ts
+++ src/data/usecases/db-add-account.spec.ts
@@ -1,12 +1,17 @@
 import { AccountModel, AddAccount, addAccountModel, AddAccountRepository, Hasher } from "./db-add-account-protocols";
 
-class HasherStub implements Hasher {
-    async hash(value: string): Promise<string> {
-        return new Promise( resolve => resolve('hashed_password'));
+const makeHasher = () => {
+    class HasherStub implements Hasher {
+        async hash(value: string): Promise<string> {
+            return new Promise( resolve => resolve('hashed_password'));
+        }
     }
+
+    return new HasherStub();
 }
 
-class AddAccountRepositoryStub implements AddAccountRepository {
+const makeAddAccountRepository = () => {
+    class AddAccountRepositoryStub implements AddAccountRepository {
     async add(accountData: addAccountModel): Promise<AccountModel> {
         return new Promise(resolve => resolve( {
             id: 'valid_id',
@@ -18,33 +23,60 @@ class AddAccountRepositoryStub implements AddAccountRepository {
     }
 }
 
-class DBAddAccount implements AddAccount{
-    constructor (private addAccountRepository: AddAccountRepository, private hasher: Hasher) {
-        this.addAccountRepository = addAccountRepository;
-        this.hasher = hasher;
-    }
+return new AddAccountRepositoryStub();
+}
+
+const makeSut = () => {
+    const hasherStub = makeHasher();
+    const addAccountRepositoryStub = makeAddAccountRepository();
+
+    class DBAddAccount implements AddAccount{
     
-    add(account: addAccountModel): Promise<AccountModel> {
-        const hashedPassword = this.hasher.hash(account.password);
-        return new Promise( resolve => resolve({
-            id: 'any_id',
-            password: hashedPassword,
-            ...account
-        }))
+        constructor (private addAccountRepository: AddAccountRepository, private hasher: Hasher) {
+            this.addAccountRepository = addAccountRepository;
+            this.hasher = hasher;
+        }
+    
+        async add(account: addAccountModel): Promise<AccountModel> {
+            const hashedPassword = await this.hasher.hash(account.password);
+            return this.addAccountRepository.add(account);
+        }
+    
+    }
+
+    const dbAddAccount = new DBAddAccount(addAccountRepositoryStub, hasherStub);
+
+    return {
+        sut: dbAddAccount,
+        hasherStub,
+        addAccountRepositoryStub
     }
 }
+
+const makeFakeAccountData = ():addAccountModel => ({
+        name: 'valid_name',
+        email: 'valid_email',
+        password: 'valid_password'
+})
 
 
 describe('DBAddAccount', () => {
     it('Shold call Hasher with correct password', async () => {
-        const hasherStub = new HasherStub();
-        const sut = new DBAddAccount(new AddAccountRepositoryStub, hasherStub);
+        const {sut, hasherStub} = makeSut();
         const encryptSpy = jest.spyOn(hasherStub, 'hash');
-        await sut.add({
+        await sut.add(makeFakeAccountData());
+        expect(encryptSpy).toHaveBeenCalledWith('valid_password');
+    });
+
+    it('Shold call AddAccountRepository with correct credentials', async () => {
+        const {sut, addAccountRepositoryStub} = makeSut();
+        const addSpy = jest.spyOn(addAccountRepositoryStub, 'add');
+        await sut.add(makeFakeAccountData())
+        expect(addSpy).toHaveBeenCalledWith({
             name: 'valid_name',
             email: 'valid_email',
             password: 'valid_password'
         })
-        expect(encryptSpy).toHaveBeenCalledWith('valid_password')
     });
+
 });
