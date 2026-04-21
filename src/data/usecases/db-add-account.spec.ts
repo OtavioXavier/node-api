3a31cd0205a30a4c2cb65665a4bbaf4d1ecb8e14
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-04-21 11:59:55 -0300

    refactor: corrigalinhamento

--- src/data/usecases/db-add-account.spec.ts
+++ src/data/usecases/db-add-account.spec.ts
@@ -1,75 +1,85 @@
 import { DBAddAccount } from "./db-add-account";
-import { AccountModel, addAccountModel, AddAccountRepository, Hasher } from "./db-add-account-protocols";
+import {
+  AccountModel,
+  addAccountModel,
+  AddAccountRepository,
+  Hasher,
+} from "./db-add-account-protocols";
 
-const makeHasher = () => {
-    class HasherStub implements Hasher {
-        async hash(value: string): Promise<string> {
-            return new Promise( resolve => resolve('hashed_password'));
-        }
+const makeHasher = (): Hasher => {
+  class HasherStub implements Hasher {
+    async hash(value: string): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_password"));
     }
+  }
 
-    return new HasherStub();
-}
+  return new HasherStub();
+};
 
-const makeAddAccountRepository = () => {
-    class AddAccountRepositoryStub implements AddAccountRepository {
+const makeAddAccountRepository = (): AddAccountRepository => {
+  class AddAccountRepositoryStub implements AddAccountRepository {
     async add(accountData: addAccountModel): Promise<AccountModel> {
-        return new Promise(resolve => resolve(makeFakeAccount())
-        )
+      return new Promise((resolve) => resolve(makeFakeAccount()));
     }
-}
+  }
 
-return new AddAccountRepositoryStub();
-}
+  return new AddAccountRepositoryStub();
+};
 
-const makeSut = () => {
-    const hasherStub = makeHasher();
-    const addAccountRepositoryStub = makeAddAccountRepository();
+type Sut = {
+  sut: DBAddAccount;
+  hasherStub: Hasher;
+  addAccountRepositoryStub: AddAccountRepository;
+};
 
-    const dbAddAccount = new DBAddAccount(addAccountRepositoryStub, hasherStub);
+const makeSut = (): Sut => {
+  const hasherStub = makeHasher();
+  const addAccountRepositoryStub = makeAddAccountRepository();
 
-    return {
-        sut: dbAddAccount,
-        hasherStub,
-        addAccountRepositoryStub
-    }
-}
+  const dbAddAccount = new DBAddAccount(addAccountRepositoryStub, hasherStub);
 
-const makeFakeAccountData = ():addAccountModel => ({
-        name: 'valid_name',
-        email: 'valid_email',
-        password: 'valid_password'
-})
+  return {
+    sut: dbAddAccount,
+    hasherStub,
+    addAccountRepositoryStub,
+  };
+};
 
-const makeFakeAccount = ():AccountModel => ({
-        id: 'valid_id',
-        name: 'valid_name',
-        email: 'valid_email',
-        password: 'hashed_password'
-})
+const makeFakeAccountData = (): addAccountModel => ({
+  name: "valid_name",
+  email: "valid_email",
+  password: "valid_password",
+});
 
-describe('DBAddAccount', () => {
-    it('Shold call Hasher with correct password', async () => {
-        const {sut, hasherStub} = makeSut();
-        const encryptSpy = jest.spyOn(hasherStub, 'hash');
-        await sut.add(makeFakeAccountData());
-        expect(encryptSpy).toHaveBeenCalledWith('valid_password');
+const makeFakeAccount = (): AccountModel => ({
+  id: "valid_id",
+  name: "valid_name",
+  email: "valid_email",
+  password: "hashed_password",
+});
+
+describe("DBAddAccount", () => {
+  it("Shold call Hasher with correct password", async () => {
+    const { sut, hasherStub } = makeSut();
+    const encryptSpy = jest.spyOn(hasherStub, "hash");
+    await sut.add(makeFakeAccountData());
+    expect(encryptSpy).toHaveBeenCalledWith("valid_password");
+  });
+
+  it("Shold call AddAccountRepository with correct credentials", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+    const addSpy = jest.spyOn(addAccountRepositoryStub, "add");
+    await sut.add(makeFakeAccountData());
+    expect(addSpy).toHaveBeenCalledWith({
+      name: "valid_name",
+      email: "valid_email",
+      password: "hashed_password",
     });
+  });
 
-    it('Shold call AddAccountRepository with correct credentials', async () => {
-        const {sut, addAccountRepositoryStub} = makeSut();
-        const addSpy = jest.spyOn(addAccountRepositoryStub, 'add');
-        await sut.add(makeFakeAccountData())
-        expect(addSpy).toHaveBeenCalledWith({
-            name: 'valid_name',
-            email: 'valid_email',
-            password: 'hashed_password'
-        })
-    });
-
-    it('Shold return a created account on success', async () => {
-        const {sut} = makeSut();
-        const createdAccount = await sut.add(makeFakeAccountData());
-        expect(createdAccount).toEqual(makeFakeAccount());
-    });
+  it("Shold return a created account on success", async () => {
+    const { sut } = makeSut();
+    const createdAccount = await sut.add(makeFakeAccountData());
+    expect(createdAccount).toEqual(makeFakeAccount());
+  });
 });
