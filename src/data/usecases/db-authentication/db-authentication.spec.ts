431e618ecbbed327c208d4585e5e9c46599c777a
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-04-21 13:27:35 -0300

    refactor: passa a usar factory design pattern

--- src/data/usecases/db-authentication/db-authentication.spec.ts
+++ src/data/usecases/db-authentication/db-authentication.spec.ts
@@ -1,39 +1,43 @@
+import { DBAuthentication } from "./db-authentication";
 import {
   AccountModel,
-  Authentication,
-  AuthenticationModel,
   LoadAccountByEmailRepository,
 } from "./db-authentication-protocols";
 
-class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
-  async loadByEmail(email: string): Promise<AccountModel> {
-    const fakeAccount: AccountModel = {
-      id: "valid_id",
-      name: "valid_name",
-      email: "valid_email",
-      password: "valid_password",
-    };
-    return new Promise((resolve) => resolve(fakeAccount));
+const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+    async loadByEmail(email: string): Promise<AccountModel> {
+      const fakeAccount: AccountModel = {
+        id: "valid_id",
+        name: "valid_name",
+        email: "valid_email",
+        password: "valid_password",
+      };
+      return new Promise((resolve) => resolve(fakeAccount));
+    }
   }
-}
+  return new LoadAccountByEmailRepositoryStub();
+};
 
-export class DBAuthentication implements Authentication {
-  constructor(
-    private readonly loadAccountByRepository: LoadAccountByEmailRepository,
-  ) {
-    this.loadAccountByRepository = loadAccountByRepository;
-  }
-  async auth(credentials: AuthenticationModel): Promise<string> {
-    await this.loadAccountByRepository.loadByEmail(credentials.email);
-    return new Promise((resolve) => resolve("valid_token"));
-  }
-}
+type Sut = {
+  sut: DBAuthentication;
+  loadEmailRepository: LoadAccountByEmailRepository;
+};
+
+const makeSut = (): Sut => {
+  const loadEmailRepository = makeLoadAccountByEmailRepository();
+  const sut = new DBAuthentication(loadEmailRepository);
+
+  return {
+    sut,
+    loadEmailRepository,
+  };
+};
 
 describe("DBAuthentication", () => {
   it("Shold call LoadAccountByRepository with correct email", async () => {
-    const loadEmailByRepositoryStub = new LoadAccountByEmailRepositoryStub();
-    const sut = new DBAuthentication(loadEmailByRepositoryStub);
-    const spy = jest.spyOn(loadEmailByRepositoryStub, "loadByEmail");
+    const { sut, loadEmailRepository } = makeSut();
+    const spy = jest.spyOn(loadEmailRepository, "loadByEmail");
     await sut.auth({ email: "valid@email.com", password: "valid_password" });
     expect(spy).toHaveBeenCalledWith("valid@email.com");
   });
