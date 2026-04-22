a2a6092cf8a6230d6d3e8fa2db71f1dd0489c2c9
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-04-22 01:21:05 -0300

    refactor: move JwtAdapter para o próprio arquivo

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { Encrypter } from "@/data/protocols/criptography/encrypter";
+import { JwtAdapter } from "./jwt-adapter";
 
 jest.mock("jsonwebtoken", () => ({
   async sign(): Promise<string> {
@@ -8,16 +8,6 @@ jest.mock("jsonwebtoken", () => ({
 }));
 
 const secret = "secret";
-
-export class JwtAdapter implements Encrypter {
-  constructor(private readonly secret: string) {
-    this.secret = secret;
-  }
-  async encrypt(value: string): Promise<string> {
-    const accessToken = jwt.sign({ id: value }, this.secret);
-    return new Promise((resolve) => resolve(accessToken));
-  }
-}
 
 describe("JWT Adapter", () => {
   it("Shold call sign with correct id", async () => {
---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+import { Encrypter } from "@/data/protocols/criptography/encrypter";
+
+export class JwtAdapter implements Encrypter {
+  constructor(private readonly secret: string) {
+    this.secret = secret;
+  }
+  async encrypt(value: string): Promise<string> {
+    const accessToken = jwt.sign({ id: value }, this.secret);
+    return new Promise((resolve) => resolve(accessToken));
+  }
+}
