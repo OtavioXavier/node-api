e6e110e32545d4c093380e8433710b0629f91069
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-04-22 00:36:59 -0300

    feat: garant que BcryptAdapter chama hash com senha e salt

---
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.spec.ts
@@ -0,0 +1,28 @@
+import { Hasher } from "@/data/protocols/criptography/hasher";
+import bcrypt from "bcrypt";
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
+const salt = 12;
+
+export class BcryptAdapter implements Hasher {
+  constructor(private readonly salt: number) {
+    this.salt = salt;
+  }
+  async hash(value: string): Promise<string> {
+    const hash = bcrypt.hash(value, salt);
+    return hash;
+  }
+}
+
+describe("Bcrypt Adapter", () => {
+  it("Shold call hash with the correct password and salt", async () => {
+    const sut = new BcryptAdapter(salt);
+    const spy = jest.spyOn(bcrypt, "hash");
+    await sut.hash("valid_password");
+    expect(spy).toHaveBeenCalledWith("valid_password", salt);
+  });
+});
