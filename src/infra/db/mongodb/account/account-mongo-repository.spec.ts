a8db4933ff566592cdf83c2dfa2b66dd1cd5cbe0
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-04-25 13:10:42 -0300

    feat: garante que AccountMongoRepository retorna uma conta ao loadByEmail ter sucesso

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -4,23 +4,31 @@ import {
   AddAccountRepository,
 } from "@/data/usecases/db-add-account/db-add-account-protocols";
 import { MongoHelper } from "../helpers/mongo-helper";
+import { LoadAccountByEmailRepository } from "@/data/usecases/db-authentication/db-authentication-protocols";
+import { Collection } from "mongodb";
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
   async add(accountData: addAccountModel): Promise<AccountModel> {
-    return new Promise((resolve) =>
-      resolve({
-        id: "any_id",
-        name: "any_name",
-        email: "any_email",
-        password: "hashed_password",
-      }),
-    );
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    const result = await accountCollection.insertOne(accountData);
+    const account = await accountCollection.findOne({ _id: result.insertedId });
+    return MongoHelper.map(account);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    const account = await (await accountCollection).findOne({ email });
+    return account && MongoHelper.map(account);
   }
 }
 
 const makeSut = (): AccountMongoRepository => {
   return new AccountMongoRepository();
 };
+
+let accountCollection: Collection;
 
 describe("AccountMongoRepository", () => {
   beforeAll(async () => {
@@ -32,7 +40,7 @@ describe("AccountMongoRepository", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts");
+    accountCollection = await MongoHelper.getCollection("accounts");
     await accountCollection.deleteMany({});
   });
 
@@ -40,12 +48,26 @@ describe("AccountMongoRepository", () => {
     const sut = makeSut();
     const account = await sut.add({
       name: "any_name",
-      email: "any_email",
+      email: "any_email@mail.com",
       password: "valid_password",
     });
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
     expect(account.name).toEqual("any_name");
-    expect(account.email).toEqual("any_email");
+    expect(account.email).toEqual("any_email@mail.com");
+  });
+
+  it("Shold return account if loadByEmail tiver sucesso", async () => {
+    const sut = makeSut();
+    await accountCollection.insertOne({
+      name: "any_name",
+      email: "any_email@mail.com",
+      password: "valid_password",
+    });
+    const account = await sut.loadByEmail("any_email@mail.com");
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toEqual("any_name");
+    expect(account.email).toEqual("any_email@mail.com");
   });
 });
