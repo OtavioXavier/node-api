c81cc10cdaed3e5605b4ebabd185ce85edbb26a6
OtavioXavier <otavioxaviermarinho@gmail.com>
2026-04-25 22:52:21 -0300

    refactor: usa factory pattern para inversão de dependencia para dado da conta

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -1,6 +1,7 @@
 import { Collection } from "mongodb";
 import { AccountMongoRepository } from "./account-mongo-repository";
 import { MongoHelper } from "../helpers/mongo-helper";
+import { addAccountModel } from "@/domain/usecases/add-account";
 
 const makeSut = (): AccountMongoRepository => {
   return new AccountMongoRepository();
@@ -22,13 +23,17 @@ describe("AccountMongoRepository", () => {
     await accountCollection.deleteMany({});
   });
 
-  it("Shold return account on add with success", async () => {
-    const sut = makeSut();
-    const account = await sut.add({
+  const makeAccountData = (): addAccountModel => {
+    return {
       name: "any_name",
       email: "any_email@mail.com",
       password: "valid_password",
-    });
+    };
+  };
+
+  it("Shold return account on add with success", async () => {
+    const sut = makeSut();
+    const account = await sut.add(makeAccountData());
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
     expect(account.name).toEqual("any_name");
@@ -37,38 +42,29 @@ describe("AccountMongoRepository", () => {
 
   it("Shold return account if loadByEmail tiver sucesso", async () => {
     const sut = makeSut();
-    await accountCollection.insertOne({
-      name: "any_name",
-      email: "any_email@mail.com",
-      password: "valid_password",
-    });
-    const account = await sut.loadByEmail("any_email@mail.com");
+    const accountData = makeAccountData();
+    await accountCollection.insertOne(accountData);
+    const account = await sut.loadByEmail(accountData.email);
     expect(account).toBeTruthy();
     expect(account.id).toBeTruthy();
-    expect(account.name).toEqual("any_name");
-    expect(account.email).toEqual("any_email@mail.com");
+    expect(account.name).toEqual(accountData.name);
+    expect(account.email).toEqual(accountData.email);
   });
 
   it("Shold return nyll if loadByEmail falhar", async () => {
     const sut = makeSut();
-    await accountCollection.insertOne({
-      name: "any_name",
-      email: "any_email@mail.com",
-      password: "valid_password",
-    });
+    const accountData = makeAccountData();
+    await accountCollection.insertOne(accountData);
     const account = await sut.loadByEmail("wrong_email@mail.com");
     expect(account).toBeNull();
   });
 
   it("Shold update the correct account on updateAccessToken", async () => {
     const sut = makeSut();
-    await accountCollection.insertOne({
-      name: "any_name",
-      email: "any_email@mail.com",
-      password: "valid_password",
-    });
+    const accountData = makeAccountData();
+    await accountCollection.insertOne(accountData);
     const { _id } = await accountCollection.findOne({
-      email: "any_email@mail.com",
+      email: accountData.email,
     });
     await sut.updateAccessToken(_id.toString(), "access_token");
     const account = await accountCollection.findOne({ _id });
